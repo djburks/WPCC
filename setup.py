82326b0ef3a1d9a48b12c1bc2d9f9ec0aba388f5
djburks <davidburks@my.unt.edu>
2016-06-12 22:05:19 -0500

    distutils

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name='WPCC',
+	version='0.1a',
+	py_modules=['wpcc'],
+	author='David Burks',
+	author_email='davidburks@my.unt.edu',
+	url='https://github.com/djburks/WPCC',
+	)
+
+
